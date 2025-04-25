82e140280c8881e8c112e0c72998da576a6a5db7
aayush <aayushdoesdev@gmail.com>
2025-04-25 18:24:55 +0530

    added profile

---
+++ src/stores/profile.js
@@ -0,0 +1,26 @@
+import { makeRequest } from "@/requests/requests";
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+export const useProfileStore = defineStore('profile', () => {
+    const endpoint = "profile"
+
+    const profile = ref({})
+
+    const getProfile = async() => {
+        try {
+            const response = await makeRequest(endpoint, "GET")
+            if(response.data){
+                profile.value = response.data
+            }
+        } catch (error) {
+            console.log("This is from profile", error)
+        }
+    }
+
+    getProfile()
+
+    return{
+        profile
+    }
+})
